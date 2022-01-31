88d655e02256c6cb8c32396a7cc9f5a435f38c7b
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-01-31 12:49:18 +0500

    Expense Form & Filter Added!

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 function Expenses({ expenses }) {
+  const [filteredYear, setFilteredYear] = useState(2022);
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
