89f4c598d412d6804c4de8619fbcb0c54e8fbca2
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-02-03 23:49:13 +0500

    Initial Add New Expense Button Added

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -59,6 +59,7 @@ const ExpenseForm = (props) => {
     // console.log(expenseData);
     props.onSaveExpenseData(expenseData);
   };
+
   return (
     <form onSubmit={formHandler}>
       <div className="new-expense__controls">
@@ -94,6 +95,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={props.onCancel}>Cancel</button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
