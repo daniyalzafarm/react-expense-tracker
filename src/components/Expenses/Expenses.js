81ea43f135a785b859960369dc39c9bc4c551b0a
Muhammad Daniyal Zafar <daniyalzafar836@gmail.com>
2022-02-03 23:41:29 +0500

    Move Expenses List to a New Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses({ expenses }) {
   const [filteredYear, setFilteredYear] = useState(2020);
@@ -15,31 +15,33 @@ function Expenses({ expenses }) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No Expenses Found</p>;
+  /*  ---- Shifted to New Component <ExpensesList /> ---- */
+  // let expensesContent = <p>No Expenses Found</p>;
 
-  if (filteredExpenses.length > 0) {
-    {
-      expensesContent = filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ));
-    }
-  }
+  // if (filteredExpenses.length > 0) {
+  //   {
+  //     expensesContent = filteredExpenses.map((expense) => (
+  //       <ExpenseItem
+  //         key={expense.id}
+  //         title={expense.title}
+  //         amount={expense.amount}
+  //         date={expense.date}
+  //       />
+  //     ));
+  //   }
+  // }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesList expenses={filteredExpenses} />
 
-      {/* Use to Display Filtered Items of an array */}
-      {expensesContent}
+      {/* ---- Use to Display Filtered Items of an array ---- */}
+      {/* {expensesContent} */}
 
-      {/* Used to Display Filtered Items of an array */}
+      {/* ---- Used to Display Filtered Items of an array ---- */}
       {/* {filteredExpenses.length === 0 && <p>No Expenses Found</p>}
       {filteredExpenses.length > 0 &&
         filteredExpenses.map((expense) => (
@@ -51,7 +53,7 @@ function Expenses({ expenses }) {
           />
         ))} */}
 
-      {/* Used to Display All Items in an array */}
+      {/* ---- Used to Display All Items in an array ---- */}
       {/* {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
@@ -61,7 +63,7 @@ function Expenses({ expenses }) {
         />
       ))} */}
 
-      {/* Used to Display Individual Items */}
+      {/* ---- Used to Display Individual Items ---- */}
       {/* <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
